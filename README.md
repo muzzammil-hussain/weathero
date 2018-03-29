e515fa436c97b135ec35dea0fd2d52d5206c2fe7
Muzzammil Hussain <muzzammilz@gmail.com>
2018-03-29 16:36:32 +0500

    Test project

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a test project in React 16.
+
+It uses openweathermap.org for data. Make sure to generate API KEY if you interested to run this.
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () => (
+		<div>
+				<h1 className='title-container__title'>Weathero</h1>
+				<p className='title-container__subtitle'>Find out temprature, conditions and more...</p>
+		</div>
+	);
+
+export default Titles;
